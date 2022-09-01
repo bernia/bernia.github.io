1ee03bf3aacfad730c042eb548174ce61a6d6641
Javier Martinez Bernia <javibernia@gmail.com>
2022-09-01 09:55:58 +0200

    Personal website first main commit

---
+++ js/commands.js
@@ -0,0 +1,87 @@
+var password = "1337";
+var linkedin = "https://www.linkedin.com/in/javier-martinez-bernia/";
+var github = "https://github.com/bernia/";
+var email = 'mailto:javibernia@gmail.com';
+var epilepsy = 'https://doi.org/10.1007/978-3-031-13321-3_46'
+var covid = 'https://doi.org/10.1007/978-3-031-13321-3_49'
+
+
+whois = [
+  "<br>",
+  "Hey, I'm Javier!👋",
+  "I am a computer engineer passionate about Artificial Intelligence and",
+  "Computer Science in general. One of the main things that motivates",
+  "me is to find AI solutions for current problems. ",
+  "I am fascinated by the field of Deep Learning, which I have been",
+  "digging into for the last few years. Computer Vision is what attracts ",
+  "me the most, but I am also interested in other fields such as Natural",
+  "Language Processing or Speech Recognition. ",
+  "I have been working in a research center developing AI solutions for health",
+  " related topics, such as epilepsy detection and lung X-ray analysis. ",
+  "Currently, I am looking to join a company to continue applying AI to",
+  "real-world problems and expand my knowledge.",
+  "<br>"
+];
+
+
+social = [
+  "<br>",
+  'linkedin       <a href="' + linkedin + '" target="_blank">linkedin/javiermartinezbernia' + "</a>",
+  'github         <a href="' + github + '" target="_blank">github/bernia' + "</a>",
+  "<br>"
+];
+
+
+projects = [
+  "<br>",
+  "Still curating... most projects are offline, on GitHub, or confidential.",
+  "<br>"
+];
+
+
+help = [
+  "<br>",
+  '<span class="command">whois</span>          Who is Bernia?',
+  '<span class="command">social</span>         Display social networks',
+  '<span class="command">projects</span>       View coding projects',
+  '<span class="command">publications</span>   View my publications',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           You obviously already know what this does',
+  '<span class="command">email</span>          Email address',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">credits</span>        Show the credits',
+  '<span class="command">banner</span>         Display the COOLEST header ever',
+  "<br>",
+];
+
+
+banner = [
+  "         ___   ____",
+  "       /' --;^/ ,-_\\     \\ | /       ",
+  "      / / --o\\ o-\\ \\\\   --(_)--   ",
+  "     /-/-/|o|-|\\-\\\\|\\\\   / | \\  _                     _       ",
+  "      '`  ` |-|   `` '         | |                   (_)      ",
+  "            |-|                | |__   ___ _ __ _ __  _  __ _ ",
+  "            |-|O               | '_ \\ / _ \\ '__| '_ \\| |/ _` |",
+  "            |-(\\,__            | |_) |  __/ |  | | | | | (_| |",
+  "         ...|-|\\--,\\_....      |_.__/ \\___|_|  |_| |_|_|\\__,_|   © 2022",
+  "      ,;;;;;;;;;;;;;;;;;;;;;;,.   ",
+  "~~,;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;,~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~",
+  '<span class="color2">Welcome to my web terminal.</span>',
+  "<span class=\"color2\">Type</span> <span class=\"command\">'help'</span><span class=\"color2\"> to see the available commands.</span>",
+];
+
+
+credits = [
+  "<br>",
+  "This website is inspired in ForrestKnight's website, FKcodes.com",
+  "<br>"
+]
+
+
+publications = [
+  "<br>",
+  'Automatic Detection of Epileptic Seizures with Recurrent and Convolutional Neural Networks @ ICIAP 2022 Workshops <a href="' + epilepsy + '" target="_blank">DOI' + "</a>",
+  'Detection of Pulmonary Conditions Using the DeepHealth Framework @ ICIAP 2022 Workshops <a href="' + covid + '" target="_blank">DOI' + "</a>",
+  "<br>"
+];
